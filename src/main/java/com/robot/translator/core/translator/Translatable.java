539e8e0af4ec256aa8a8069c93ffb36d77498cfa
luozhan <luozhan@asiainfo.com>
2020-04-30 13:20:06 +0800

    校正

--- src/main/java/com/robot/translator/core/translator/Translatable.java
+++ src/main/java/com/robot/translator/core/translator/Translatable.java
@@ -17,10 +17,10 @@ public interface Translatable {
      *
      * @param groupValue 字典group值，不用可忽略（如静态字典需要先通过group值确定范围，再根据code值得到对应的value）
      * @param origin     待翻译的原始值（对应字典code属性）
-     * @param dicConfig  字典注解，可获取属性配置
+     * @param dictConfig  字典注解，可获取属性配置
      * @param dictClass  字典class
      * @return 字典value，可以返回null值，翻译时会处理（如果为null则显示原始值）
      */
-    String translate(String groupValue, String origin, Dictionary dicConfig, Class dictClass);
+    String translate(String groupValue, String origin, Dictionary dictConfig, Class dictClass);
 
 }
